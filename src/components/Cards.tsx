9c3febb74d1fd62d6d2f47382d9ea17a5d0515bd
femakin <santosfemakin@yahoo.com>
2024-02-22 13:47:18 +0100

    Modified Mobile design for Deals and revenue

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -36,7 +36,8 @@ function Cards(): JSX.Element {
 
     return (
 
-        <section className='  block 2xl:flex xl:flex md:block lg:flex flex-wrap  flex-1 '>
+        // <section className='  block 2xl:flex xl:flex md:block lg:flex flex-wrap  flex-1 '>
+        <section className='lg:flex-row md:flex-row xl:flex-row 2xl:flex-row flex-col   flex 2xl:w-1/2 md:w-1/2 xl:w-1/2 lg:w-1/2 flex-wrap '>
             {transformedData.map((metric, index) => (
                 <Card className=' p-6 sm:w-auto border  w-auto xl:w-72 2xl:w-72  lg:w-72 h-42' index={index}
                     key={metric.title}
@@ -46,13 +47,9 @@ function Cards(): JSX.Element {
                     style={index === 0 ? {
                         background: 'conic-gradient(from 240deg at 50% 20%, #e8fedd, #e3fed0, #dffdc4, #defdb7)',
                         borderRadius: '8px',
-
-                        // boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-
                     } : {
                         background: 'white',
                         borderRadius: '8px',
-                        // boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
                     }}
                 />
             ))}
